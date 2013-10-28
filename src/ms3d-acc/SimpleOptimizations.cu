3ffa546d1bbecf117236246dfe84367fc0c047be
soulsheng <soulsheng@qq.com>
2013-10-28 20:59:31 +0800

    合并权重， 矩阵无法满足合并

--- src/ms3d-acc/SimpleOptimizations.cu
+++ src/ms3d-acc/SimpleOptimizations.cu
@@ -48,9 +48,9 @@ transformVectorByMatrix4( const  float4 *pInput, const int *pIndex, float4 *pMat
 		}
 }
 
-// 等间隔静态网格，骨骼为2
+// 等间隔静态网格，骨骼为2  合并：顶点、骨骼索引权重
 __global__ void
-transformVectorByMatrix4Cross( const  float4 *pInput, const int2 *pIndex, float4 *pMatrix, float4 *pOutput,  int sizeMax,  const float *pWeight)
+transformVectorByMatrix4( const  float4 *pInput, const int2 *pIndex, float4 *pMatrix, float4 *pOutput,  int sizeMax, const float2 *pWeight)
 {
 	const int indexBase = ( gridDim.x * blockIdx.y + blockIdx.x ) * blockDim.x + threadIdx.x;
 
@@ -61,7 +61,7 @@ transformVectorByMatrix4Cross( const  float4 *pInput, const int2 *pIndex, float4
 
 		{
 			int offset = pIndex[index].x * 4 ;
-			float weight = pWeight[index] ;
+			float weight = pWeight[index].x ;
 			float4 weight4 = make_float4( weight,weight,weight,weight ) ;
 
 			float4 m0 = pMatrix[offset+0] * weight4 ;
@@ -70,7 +70,7 @@ transformVectorByMatrix4Cross( const  float4 *pInput, const int2 *pIndex, float4
 			float4 m3 = pMatrix[offset+3] * weight4 ;
 
 			offset = pIndex[index].y * 4 ;
-			weight = 1 - weight ;
+			weight = pWeight[index].y ;
 			weight4 = make_float4( weight, weight, weight, weight ) ;
 
 			m0 += pMatrix[offset+0] * weight4 ;
@@ -223,73 +223,6 @@ transformVectorByMatrix4One( const Vector4 *pInput, const Vector1i *pIndex, Vect
 		}
 }
 
-/* 坐标矩阵变换
-pVertex  : 坐标
-pMatrix : 矩阵
-*/
-template<typename F4>
-__device__ void transformVec3ByMatrix4(F4* pVertexIn, float1 pMatrix[], F4* pVertexOut)
-{
-	F4 vertexIn = *pVertexIn;
-	F4 vertexOut;
-	vertexOut.x = vertexIn.x * pMatrix[0].x + vertexIn.y * pMatrix[1].x + vertexIn.z * pMatrix[2].x + pMatrix[3].x ; 
-	vertexOut.y = vertexIn.x * pMatrix[1*4+0].x + vertexIn.y * pMatrix[1*4+1].x + vertexIn.z * pMatrix[1*4+2].x + pMatrix[1*4+3].x  ; 
-	vertexOut.z = vertexIn.x * pMatrix[2*4+0].x + vertexIn.y * pMatrix[2*4+1].x + vertexIn.z * pMatrix[2*4+2].x + pMatrix[2*4+3].x  ;
-	*pVertexOut = vertexOut;
-}
-template<typename F4>
-__device__ void transformVec3ByMatrix4(F4* pVertexIn, F4 pMatrix[], F4* pVertexOut)
-{
-	F4 vertexIn = *pVertexIn;
-	F4 vertexOut;
-	vertexOut.x = vertexIn.x * pMatrix[0].x + vertexIn.y * pMatrix[0].y + vertexIn.z * pMatrix[0].z + pMatrix[0].w ; 
-	vertexOut.y = vertexIn.x * pMatrix[1].x + vertexIn.y * pMatrix[1].y + vertexIn.z * pMatrix[1].z + pMatrix[1].w  ; 
-	vertexOut.z = vertexIn.x * pMatrix[2].x + vertexIn.y * pMatrix[2].y + vertexIn.z * pMatrix[2].z + pMatrix[2].w  ;
-	*pVertexOut = vertexOut;
-}
-
-template<typename F4>
-__device__ void transformVec3ByMatrix4_f4(F4* pVertexIn, float4 pMatrix[], F4* pVertexOut)
-{
-	F4 vertexIn = *pVertexIn;
-	F4 vertexOut;
-	vertexOut.x = vertexIn.x * pMatrix[0].x + vertexIn.y * pMatrix[0].y + vertexIn.z * pMatrix[0].z + pMatrix[0].w ; 
-	vertexOut.y = vertexIn.x * pMatrix[1].x + vertexIn.y * pMatrix[1].y + vertexIn.z * pMatrix[1].z + pMatrix[1].w  ; 
-	vertexOut.z = vertexIn.x * pMatrix[2].x + vertexIn.y * pMatrix[2].y + vertexIn.z * pMatrix[2].z + pMatrix[2].w  ;
-	*pVertexOut = vertexOut;
-}
-
-	// 按矩阵索引
-template<typename F4>
-__device__ void indexByFloat44( F4* pBuffer , F4* pMat , int index )
-	{
-		for(int j=0; j<MATRIX_SIZE_LINE; j++){
-			pMat[j] = pBuffer[index * MATRIX_SIZE_LINE + j];
-		}
-	}
-
-
-__global__ void updateVectorByMatrix(float4* pVertexIn, int size, float1* pMatrix, float4* pVertexOut)
-{
-	const int indexBase = ( gridDim.x * blockIdx.y + blockIdx.x ) * blockDim.x + threadIdx.x;
-
-		for( int i=indexBase; i<size; i+=blockDim.x * gridDim.x ){
-
-		float4   matrix[MATRIX_SIZE_LINE];
-
-		// 读取操作数：初始的顶点坐标
-		float4   vertexIn = pVertexIn[i];
-
-		// 读取操作数：顶点对应的矩阵
-		int      matrixIndex = int(vertexIn.w + 0.5);// float to int
-
-		
-		indexByFloat44( (float4*)pMatrix, matrix, matrixIndex );
-
-		// 执行操作：对坐标执行矩阵变换，得到新坐标
-		transformVec3ByMatrix4( &vertexIn, matrix, pVertexOut+i);
-	}//for
-}
 
 extern "C" bool
 runCUDADevice( const float *pInput, const int *pIndex, float *pMatrix, float *pOutput,  int sizeMax,  const float *pWeight )
@@ -314,7 +247,7 @@ runCUDADevice( const float *pInput, const int *pIndex, float *pMatrix, float *pO
     transformVectorByMatrix4One<<< grid, block >>>( (FLOAT4*)pInput, (INT1*)pIndex, (FLOAT4*)pMatrix, (FLOAT4*)pOutput, sizeMax, (FLOAT1*)pWeight );
 #endif
 #elif SIZE_PER_BONE==2
-    transformVectorByMatrix4Cross<<< grid, block >>>( (FLOAT4*)pInput, (int2*)pIndex, (FLOAT4*)pMatrix, (FLOAT4*)pOutput, sizeMax, (float*)pWeight );
+    transformVectorByMatrix4<<< grid, block >>>( (FLOAT4*)pInput, (int2*)pIndex, (FLOAT4*)pMatrix, (FLOAT4*)pOutput, sizeMax, (float2*)pWeight );
 #else
     transformVectorByMatrix4<<< grid, block >>>( (FLOAT4*)pInput, (int*)pIndex, (FLOAT4*)pMatrix, (FLOAT4*)pOutput, sizeMax, (float*)pWeight );
 #endif
